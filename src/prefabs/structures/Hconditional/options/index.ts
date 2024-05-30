82b241d5a44969edec58eccdffa6fdfe55d6a140
Alvin Acosta <alvinfloresacosta@gmail.com>
2024-05-30 10:45:59 +0800

    initial commit

---
+++ src/prefabs/structures/Hconditional/options/index.ts
@@ -0,0 +1,80 @@
+import { option, showIf, toggle, variable } from '@betty-blocks/component-sdk';
+import { advanced } from '../../advanced';
+
+export const categories = [
+  {
+    label: 'Advanced Options',
+    expanded: false,
+    members: ['dataComponentAttribute'],
+  },
+];
+
+export const hconditionalOptions = {
+  visible: toggle('Initial visibility', {
+    value: true,
+    configuration: {
+      as: 'VISIBILITY',
+      condition: showIf('type', 'EQ', 'singleRule'),
+    },
+  }),
+
+  left: variable('Left', {
+    value: [],
+    configuration: {
+      condition: showIf('type', 'EQ', 'singleRule'),
+    },
+  }),
+
+  compare: option('CUSTOM', {
+    label: 'Compare',
+    value: 'eq',
+    configuration: {
+      as: 'DROPDOWN',
+      dataType: 'string',
+      allowedInput: [
+        {
+          name: 'Equals',
+          value: 'eq',
+        },
+        {
+          name: 'Not equal',
+          value: 'neq',
+        },
+        {
+          name: 'Contains',
+          value: 'contains',
+        },
+        {
+          name: 'Does not contain',
+          value: 'notcontains',
+        },
+        {
+          name: 'Greater than',
+          value: 'gt',
+        },
+        {
+          name: 'Less than',
+          value: 'lt',
+        },
+        {
+          name: 'Greater than or equal to',
+          value: 'gteq',
+        },
+        {
+          name: 'Less than or equal to',
+          value: 'lteq',
+        },
+      ],
+      condition: showIf('type', 'EQ', 'singleRule'),
+    },
+  }),
+
+  right: variable('Right', {
+    value: [],
+    configuration: {
+      condition: showIf('type', 'EQ', 'singleRule'),
+    },
+  }),
+
+  ...advanced('Hconditional'),
+};
